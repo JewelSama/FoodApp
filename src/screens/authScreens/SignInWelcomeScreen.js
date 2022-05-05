4558d1f945b7afad7534d1dd07d295dc19bbe515
JewelKahz <67759825+JewelKahz@users.noreply.github.com>
2022-05-05 11:03:25 +0100

    set up welcome screen

---
+++ src/screens/authScreens/SignInWelcomeScreen.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, TextInput, View } from 'react-native'
+import React, {useState, useRef} from 'react'
+import { colors, parameters, title } from '../../global/styles';
+import { Icon, Button, SocialIcon } from 'react-native-elements';
+import * as Animatable from 'react-native-animatable';
+
+export default function SignInWelcomeScreen (){
+    return(
+        <View>
+            
+        </View>
+    )
+};
