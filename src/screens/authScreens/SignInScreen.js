6694c343b74366183e36730655efba71eb481f89
JewelKahz <67759825+JewelKahz@users.noreply.github.com>
2022-05-05 10:53:40 +0100

    add components to screen

--- src/screens/authScreens/SignInScreen.js
+++ src/screens/authScreens/SignInScreen.js
@@ -1,8 +1,8 @@
 import { StyleSheet, Text, TextInput, View } from 'react-native'
 import React, {useState, useRef} from 'react'
 import Header from '../../components/Header';
-import { colors, title } from '../../global/styles';
-import { Icon } from 'react-native-elements';
+import { colors, parameters, title } from '../../global/styles';
+import { Icon, Button, SocialIcon } from 'react-native-elements';
 import * as Animatable from 'react-native-animatable';
 
 export default function SignInScreen() {
@@ -73,7 +73,52 @@ const TextInput2 = useRef(2)
         </Animatable.View> 
     </View>
 
+        <View style={{marginHorizontal: 20, marginTop: 30}}>
+            <Button 
+                title="SIGN IN"
+                buttonStyle={parameters.styledButton}
+                titleStyle={parameters.buttonTitle}
+            />
 
+        </View>
+        <View style={{alignItems: "center", marginTop: 15}}>
+            <Text style={{...styles.text1, textDecorationLine: "underline"}}>Forgot Password ?</Text>
+        </View>
+
+        <View style={{alignItems: "center", marginTop: 30, marginBottom: 30}}>
+            <Text style={{fontSize: 20, fontWeight: "bold"}}>OR</Text>
+        </View>
+
+        <View style={{marginHorizontal: 10, marginTop: 10}}>
+            <SocialIcon 
+                title="Sign In With Facebook"
+                button
+                type="facebook"
+                style = {styles.SocialIcon}
+                onPress={() => {}}
+            />
+        </View>
+        <View style={{marginHorizontal: 10, marginTop: 10}}>
+            <SocialIcon 
+                title="Sign In With Google"
+                button
+                type="google"
+                style = {styles.SocialIcon}
+                onPress={() => {}}
+            />
+        </View>
+
+        <View style={{ marginTop: 25, marginLeft: 20}}>
+            <Text style={{...styles.text1,}}>New on JewelFood?</Text>
+        </View>
+
+        <View style={{alignItems: 'flex-end', marginHorizontal: 20}}>
+            <Button 
+                title="Create an account"
+                buttonStyle={styles.createButton}
+                titleStyle={styles.createButtonTitle}
+            />
+        </View>
 
     </View>
   )
@@ -117,6 +162,28 @@ const styles = StyleSheet.create({
         justifyContent: "space-between",
         alignContent: "center",
         alignItems: 'center',
+    },
+    SocialIcon: {
+        borderRadius: 12,
+        height: 50,
+    },
+    createButton: {
+        backgroundColor: "white",
+        alignContent: "center",
+        justifyContent: "center",
+        borderRadius: 12,
+        borderWidth: 1,
+        borderColor: "#ff8c52",
+        height: 40,
+        paddingHorizontal: 20
+    },
+    createButtonTitle: {
+        color: "#ff8c52",
+        fontSize: 16,
+        fontWeight: "bold",
+        alignItems: "center",
+        justifyContent: "center",
+        marginTop: -3
     }
 
 })
